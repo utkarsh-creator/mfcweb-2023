f0b559543e22ed501bd322a099f44f42fd45c089
utkarsh-creator <utkarshtyagi959@gmail.com>
2024-05-30 15:48:57 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,144 @@
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 200,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#F6842A",
+    },
+    shape: {
+      type: "polygon",
+      stroke: {
+        width: 0,
+        color: "#ffff",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100,
+      },
+    },
+    opacity: {
+      value: 0.5,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 0.1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 1,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: false,
+    },
+    move: {
+      enable: true,
+      speed: 1,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+      },
+      onclick: {
+        enable: false,
+      },
+      resize: true,
+    },
+  },
+  retina_detect: true,
+});
+window.addEventListener("load", function () {
+  var preloader = document.querySelector(".preloader");
+  var otherContent = document.querySelector(".other-content");
+  otherContent.style.display = "none";
+  setTimeout(function () {
+    preloader.style.display = "none";
+    otherContent.style.display = "block";
+  }, 3000);
+});
+const facultyDesc = document.querySelector(".check");
+const infoCard = document.querySelector(".info-card");
+infoCard.addEventListener("mouseover", () => {
+  facultyDesc.style.display = "block";
+  infoCard.style.opacity = 0;
+});
+infoCard.addEventListener("mouseout", () => {
+  facultyDesc.style.display = "none";
+  infoCard.style.opacity = 1;
+});
+
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
+
+
+  function updateBoardMembers() {
+    var selectedYear = document.getElementById("year").value;
+    var board2022 = document.getElementById("board-members-2022");
+    var board2023 = document.getElementById("board-members-2023");
+
+    if (selectedYear === "2022") {
+        board2022.style.display = "flex";
+        board2023.style.display = "none";
+    } else if (selectedYear === "2023") {
+        board2022.style.display = "none";
+        board2023.style.display = "flex";
+    }
+}
